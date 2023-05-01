64f2eba6fad7c0b8c8d8c5c4d0f80a965bb3276a
FaisalMS <fis445@hotmail.com>
2023-05-01 20:58:13 +0300

    Version 0.1

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPxYx9lSHh_jOdgmDVH4ukNc6ltJIonCo",
+  authDomain: "netflix-clone-88a12.firebaseapp.com",
+  projectId: "netflix-clone-88a12",
+  storageBucket: "netflix-clone-88a12.appspot.com",
+  messagingSenderId: "637209257926",
+  appId: "1:637209257926:web:f8f623d8ea3f676eca34c7",
+};
